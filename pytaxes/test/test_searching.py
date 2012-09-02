8e5867a35134ea710e84598fb548ec1b66918821
Chris Perivolaropoulos <darksaga2006@gmail.com>
2012-09-02 23:13:52 +0300

    This is basically the first working version. My committing habits are horrible.

---
+++ pytaxes/test/test_searching.py
@@ -0,0 +1,21 @@
+import unittest
+from ..search import Parser
+from datetime import date
+import os
+from ..hashtable import HashTable, X17, index_file
+
+class TestSearchParser(unittest.TestCase):
+    def setUp(self):
+        self.static_dir = os.path.dirname(os.path.realpath(__file__)) + '/static'
+        self.ht = index_file(self.static_dir+"/cards.txt")
+
+    def test_searching(self):
+        """Many search terms of the same kind should be additive,
+        different kinds are subtractive.
+        """
+        p = Parser("dates 2010 to 2013") # 18/6/2011")
+        res = p.search(self.ht)
+        self.assertEquals("", "") # Too many to compare (nice testing habits)0
+    
+if __name__ == "__main__":
+    unittest.main()
